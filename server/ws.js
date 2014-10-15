f41dbfb7f9e77eabc3d50f11ca904fe19fe1667f
Peter Helenefors <peter@hertigcarl.se>
2014-10-15 14:03:14 +0200

    testar websocket ist för ws

--- server/ws.js
+++ server/ws.js
@@ -3,9 +3,28 @@ var ActiveClients = require('./rules.js').actives;
 var EventGenerator = require('./rules.js').EventGenerator;
 var Rules = require('./rules.js').Rules;
 
+var http = require('http');
 
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 5000});
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(5000, function() {
+    console.log((new Date()) + ' Server is listening on port 5000');
+});
+
+var WebSocketServer = require('websocket').server
+  , wss = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
 
 var games  = {
 	'bloodsuckers':30105
@@ -40,8 +59,10 @@ var ensureMessage = function(message) {
 
 }
 
-wss.on('connection', function(ws) {
+wss.on('request', function(request) {
     	console.log("Connection achieved")
+
+	    var ws = request.accept(null, request.origin);
 
     	ws.on('message', function(message) {
     		console.log("Incoming message");
